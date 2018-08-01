d7988bbca3c5c31c4f458695e2beb49451bb7330
Nick Woodward <nickwoodward5@gmail.com>
2018-08-01 11:53:07 -0700

    Working search function with properly returning error messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,11 @@
+
 var card = "<div class='card' style='width: 18rem;'> <img class='card-img-top' src='.../100px180/' alt='Card image cap'> <div class='card-body'> <h5 class='card-title'>Card title</h5> <p class='card-text'></p> <a href='#' class='btn btn-primary'>View Profile</a> </div> </div>";
 $(document).ready(function () {
+    $("#scrollButton").on("click", function () {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#searchTitle").offset().top
+        }, 2000);
+    });
     $("#searchByLocation").on("click", function (event) {
         event.preventDefault();
         var citySearched = $("#citySearchInput").val().trim()
@@ -30,24 +36,40 @@ $(document).ready(function () {
         $("#titleHere").append("<h3 class='text-light text-shadow' id='searchTitle'>EXPERIENCE</h3>")
         $("#titleHere").append("<input class='form-control mr-sm-2 w-search' type='search' placeholder='Experience' aria-label='Experience' id='experienceSearchAdvInput'>")
         $("#titleHere").append("<br>")
-        $("#titleHere").append("<button class='btn btn-outline-secondary my-2 my-sm-0' type='submit' id='advSearchBtn'>Search</button>")
+        $("#titleHere").append("<button class='btn btn-outline-light my-2 my-sm-0' type='submit' id='advSearchBtn'>Search</button>")
     });
 
     $("body").on("click", "#advSearchBtn", function () {
+        var counter = 0;
         console.log("Hello!");
         var advLocation = $("#locationSearchAdvInput").val();
         var advExperience = $("#experienceSearchAdvInput").val();
-        $.get("/api/city/" + advLocation + "/" + advExperience).then(function (res) {
+        $.get("/api/city/" + advLocation).then(function (res) {
             $(".card").remove();
             $(".errorMessage").remove();
+
             if (res.length > 0) {
+                console.log("Rs.length" + res.length)
                 for (var i = 0; i <= res.length; i++) {
-                    console.log(res[i])
-                    $("#results").append("<div class='card col-md-3' style='width: 18rem;'> <img class='card-img-top' src='" + res[i].profilePhoto + "' alt = 'user profile picture'> <div class = 'card-body'> <h5 class-'card-title'>" + res[i].name + "</h5> <p class='card-text'>" + res[i].aboutYou + "</p> <a href='localhost8080/" + res[i].userhandle + "' class='btn btn-primary'> View Profile </a> </div> </div>")
-                };
-            } else {
-                $("#results").append("<h2 style='text-align: center;' class='errorMessage'>Sorry, nobody in " + advLocation + " is offering a " + advExperience + " experience right now");
-            }
-        });
+                    console.log(res[i]);
+                    var splitArray = res[i].activities.split(",")
+                    console.log(splitArray)
+                    for (var j = 0; j <= splitArray.length; j++) {
+                        if (splitArray[j] == advExperience) {
+                            counter++
+                            console.log(advExperience);
+                            console.log(splitArray[j]);
+                            $("#results").append("<div class='card col-md-3' style='width: 18rem;'> <img class='card-img-top' src='" + res[i].profilePhoto + "' alt = 'user profile picture'> <div class = 'card-body'> <h5 class-'card-title'>" + res[i].name + "</h5> <p class='card-text'>" + res[i].aboutYou + "</p> <a href='localhost8080/" + res[i].userhandle + "' class='btn btn-primary'> View Profile </a> </div> </div>")
+                        } else if ( j === splitArray.length && counter === 0){
+                            $(".card").remove();
+                            $(".errorMessage").remove();
+                            $("#results").append("<h1 class='errorMessage' style='text-align:center;'>Sorry, no one in" + advLocation + " is offering a " + advExperience + " experience right now. Check back soon! </h1>")
+                        }
+                    } 
+                }
+            } 
+        })
     });
 });
+
+
