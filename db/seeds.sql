c4336a3c3b9fbb9a4c61d961702db27157792e4a
cutesd <kimberly@kimberlycooper.me>
2018-08-02 16:11:49 -0700

    backup

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,68 +1,181 @@
 USE `travel_mateDB`;
+{
+    "name": "Brad Stevens",
+    "email": "bstevens@hotmail.com",
+    "password": ,
+    "location": "Paris, France",
+    "interests": "Sports, Outdoors, Night Scene",
+    "about": "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.",
+    "activities": "Blah blah blah",
+    "profilePhoto": "https://images.pexels.com/photos/45882/man-crazy-funny-dude-45882.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "coverPhoto": "https://images.unsplash.com/photo-1495401246624-593eb4b920ba?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d8257fb59fbb7007ed2c200404795641&auto=format&fit=crop&w=800&q=60",
+    "username": "bradS"
+}
 
-INSERT INTO Users (name, email, password, location, interests, about, activities, profilePhoto, coverPhoto, username, createdAt, updatedAt)
-VALUES ("Brad Stevens","bstevens@hotmail.com","7777777","Paris, France","Sports, Outdoors, Night Scene",
-"Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.",
-"Blah blah blah",
-"https://images.pexels.com/photos/45882/man-crazy-funny-dude-45882.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-"https://images.unsplash.com/photo-1495401246624-593eb4b920ba?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d8257fb59fbb7007ed2c200404795641&auto=format&fit=crop&w=800&q=60",
-"bradS",now(),now());
+{
+    "name": "Simon Robben",
+    "email": "srobben@gmail.com",
+    "password": ,
+    "location": "Austin, Texas",
+    "interests": "Fine Dining, Art & Museums, Music Events, Sight Seeing",
+    "about": "Cupcake ipsum dolor sit. Amet chocolate cake tiramisu jelly beans dragée halvah tiramisu I love. Tootsie roll bear claw fruitcake chocolate I love jujubes toffee fruitcake powder. Marshmallow dessert fruitcake danish biscuit ice cream. Tiramisu lemon drops sesame snaps. Cookie cupcake muffin. Macaroon halvah oat cake caramels danish. Carrot cake halvah chocolate. Caramels danish sesame snaps sweet.",
+    "activities": "It's a blah blah kind of blah blah blah blah blah",
+    "profilePhoto": "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    "coverPhoto": "https://images.unsplash.com/photo-1504660069764-2b37e279874a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7945a4d590c31ea4887f50ab42dd6d17&auto=format&fit=crop&w=800&q=60",
+    "username": "sRobben"
+}
 
-INSERT INTO Users (name, email, password, location, interests, about, activities, profilePhoto, coverPhoto, username, createdAt, updatedAt)
-VALUES ("Simon Robben","srobben@gmail.com","666666","Austin, Texas","Fine Dining, Art & Museums, Music Events, Sight Seeing",
-"Cupcake ipsum dolor sit. Amet chocolate cake tiramisu jelly beans dragée halvah tiramisu I love. Tootsie roll bear claw fruitcake chocolate I love jujubes toffee fruitcake powder. Marshmallow dessert fruitcake danish biscuit ice cream. Tiramisu lemon drops sesame snaps. Cookie cupcake muffin. Macaroon halvah oat cake caramels danish. Carrot cake halvah chocolate. Caramels danish sesame snaps sweet.",
-"It's a blah blah kind of blah blah blah blah blah",
-"https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-"https://images.unsplash.com/photo-1504660069764-2b37e279874a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7945a4d590c31ea4887f50ab42dd6d17&auto=format&fit=crop&w=800&q=60",
-"sRobben",now(),now());
+{
+    "name": "Billy Bob",
+    "email": "billbob@gmail.com",
+    "password": "4444",
+    "location": "Paris, France",
+    "interests": "Fine Dining, Outdoors",
+    "about": "My name is Billy Bob. I am know Paris very well and will show you all the most delicious foods to eat.",
+    "activities": "hiking, paddle boating, dining",
+    "profilePhoto": "https://media.gq.com/photos/56d75ae39acdcf20275f0f4e/master/pass/1-courtesy%20of%20Gucci.jpg",
+    "coverPhoto": "https://www.musictick.com/img/slide/slide_parigi.jpg",
+    "username": "BillyBob"
+}
 
-INSERT INTO Users (name, email, password, hostTown, interests, aboutYou, activities, profilePhoto, coverPhoto, handle, createdAt, updatedAt)
-VALUES ("","","","","","","","","","",now(),now());
+{
+    "name": "Barbara",
+    "email": "babs241@yahoo.com",
+    "password": "4444",
+    "location": "San Francisco, CA",
+    "interests": "Night Scene,  Art & Museums",
+    "about": "Hello! My name is Barbara, I will  travelling to San Francisco next month and would love to see all the amazing museums, art and clubs in the city",
+    "activities": "dancing, museums and art",
+    "profilePhoto": "http://djbrecord.free.fr/barbara019.jpg",
+    "coverPhoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSnlmU5YssZUd8aAgKIG5Yl8aBsBqdv7fyJc0K5cdzKpWTLYGq",
+    "username": "babs"
+}
 
-INSERT INTO Users (name, email, password, hostTown, interests, aboutYou, activities, profilePhoto, coverPhoto, handle, createdAt, updatedAt)
-VALUES ("","","","","","","","","","",now(),now());
+{
+    "name": "Deb Rah",
+    "email": "debdeb@outlook.com",
+    "password": "4444",
+    "location": "Buenos Aires, Argentina",
+    "interests": "Outdoors, Camping",
+    "about": "My name is Deb and I live in Buenos Aires. I love everything outdoors and adventure and can show you the beautiful landscapes near by my home town.",
+    "activities": "hiking, biking, exploring",
+    "profilePhoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHAmgX2ahZ3zYUo9iP4iTfgz2Lrn2B8Jk6V1SXmFljRoZarYUUQA",
+    "coverPhoto": "https://www.zicasso.com/sites/default/files/styles/original_scaled_down/public/photos/review/1583777/braziliguassufalls-gapargentinaadventure.jpg",
+    "username": "debdeb"
+}
 
-INSERT INTO Users (name, email, password, hostTown, interests, aboutYou, activities, profilePhoto, coverPhoto, handle, createdAt, updatedAt)
-VALUES ("","","","","","","","","","",now(),now());
+{
+    "name": "Frank",
+    "email": "fbomb@aol.com",
+    "password": "4444",
+    "location": "Sydney, Australia",
+    "interests": ,
+    "about": "I want to see new things and everything Sydney has to offer",
+    "activities": "I am friendly, chill and enjoy having a good time",
+    "profilePhoto": "http://www.sinatra.com/sites/default/files/styles/news-thumbnail/public/Frank%20Sinatra%202.25.15%20site%20post.jpg?itok=bH18he2V",
+    "coverPhoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyIQ860p-8HW9rkHdCpDr3Qm4vh5IDii4cM4n4wgMW47s9v2bYeQ",
+    "username": "frankie"
+}
 
-INSERT INTO Users (name, email, password, hostTown, interests, aboutYou, activities, profilePhoto, coverPhoto, handle, createdAt, updatedAt)
-VALUES ("","","","","","","","","","",now(),now());
+{
+    "name": ,
+    "email": ,
+    "password": "4444",
+    "location": ,
+    "interests": ,
+    "about": ,
+    "activities": ,
+    "profilePhoto": ,
+    "coverPhoto": ,
+    "username": 
+}
 
-INSERT INTO Users (name, email, password, hostTown, interests, aboutYou, activities, profilePhoto, coverPhoto, handle, createdAt, updatedAt)
-VALUES ("","","","","","","","","","",now(),now());
+-- INSERT INTO register (name, email, password, handle, location, interests, activities, aboutYou, profilePhoto, coverPhoto, host, hostee) VALUES ("Michelle B.", "michelleyb123@ymail.com", "?", "@michelle", "Montreal, Québec, Canada", "outdoors and dining", "snowboarding, skiing, cooking and going out to eat", "My name is Michelle and I live in Québec. I love to play in the snow and cook delicious meals at home. I can show you near by ski and snowboard mountains to spend the day at.","https://assets.vogue.com/photos/58584b45a6984b8c49fabbb0/master/pass/michelle-ruiz.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJbvXxf_xVcV-70WGOTO7lniI3e7huHfCW_AebxCZVk_mcQFB6", true, false); 
 
-SELECT * FROM Users;
+{
+    "name": ,
+    "email": ,
+    "password": "4444",
+    "location": ,
+    "interests": ,
+    "about": ,
+    "activities": ,
+    "profilePhoto": ,
+    "coverPhoto": ,
+    "username": 
+}
 
-ALTER TABLE Messages
-ADD authorId INTEGER;
+-- INSERT INTO register (name, email, password, handle, location, interests, activities, aboutYou, profilePhoto, coverPhoto, host, hostee) VALUES ("Robert O.", "roberto@)mail.com", "?", "@robert0321", "Moscow, Russia", "drinking", "drinking activities", "Hi my name is Robert and I am coming to Moscow in August from Poland. I want to have a good time and experience all that the city has to offer", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCF9BCQDhciXJOPcT1XB5RHpxXGpJ0STjbQEWwrGyxNVsF7PAI", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr6UFU4iukLjxM4ez5Eaf088fP35O-eER_lcmNOAexf_ZsHmov", false, true);
 
-ALTER TABLE Messages
-DROP COLUMN authorId;
+{
+    "name": ,
+    "email": ,
+    "password": "4444",
+    "location": ,
+    "interests": ,
+    "about": ,
+    "activities": ,
+    "profilePhoto": ,
+    "coverPhoto": ,
+    "username": 
+}
 
+-- INSERT INTO register (name, email, password, handle, location, interests, activities, aboutYou, profilePhoto, coverPhoto, host, hostee) VALUES ("Mary Poppins", "mpoppins@gmail.com", "?", "@marypoppins", "London, England", "tea and spoons full of sugar", "tea parties and flying", "Hello I am Mary Poppins and live in London, England. I can show you all of the sights of my town. I grew up here and know all of its history.", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2SaJVA3LVxB6BdAtystS1rJgWlhH-u-8dswqtTVlfsA7tpRPq", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToWPG_bGU5Bkhk7HmA-biwzMP0jHKtAWvPY5FV7U7KXWg8FNNgDQ", true, false);
 
+{
+    "name": ,
+    "email": ,
+    "password": "4444",
+    "location": ,
+    "interests": ,
+    "about": ,
+    "activities": ,
+    "profilePhoto": ,
+    "coverPhoto": ,
+    "username": 
+}
+-- 
+INSERT INTO register (name, email, password, handle, location, interests, activities, aboutYou, profilePhoto, coverPhoto, host, hostee) VALUES ("Martin Luther", "mluther@yahoo.com", "?", "@martin", "New York City", "art", "art walks, art shows, museums", "Hi my name is Martin. I am a host  in New York City and love art and museums.", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIAaEOJ8qdWhTbk7DP-Ylr_RzYd4l1kGzGC0ZQ1fEiQvqH6iYHPA", "https://media.wired.com/photos/59443560e8e2f01290fc76ab/master/pass/iStock-501796588.jpg", true, false);
 
-SELECT * FROM Messages;
+{
+    "name": ,
+    "email": ,
+    "password": "4444",
+    "location": ,
+    "interests": ,
+    "about": ,
+    "activities": ,
+    "profilePhoto": ,
+    "coverPhoto": ,
+    "username": 
+}
 
+-- INSERT INTO register (name, email, password, handle, location, interests, activities, aboutYou, profilePhoto, coverPhoto, host, hostee) VALUES ("Mickey Mouse", "mickey123@disney.com", "?", "@mickeymouse", "Chiang Mai, Thailand", "animals", "caring for elephants, hiking, nature", "Hi I am Mickey and I am travelling to Thailand. I want to visit an Elephant Sanctuary in Chiang Mai", "https://www.emmys.com/sites/default/files/styles/show_detail/public/disney-mickey-mouse-600x600.jpg?itok=37-ghpD0", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDLYkqbEvRCTnJF4c27S5mMLD-3IMcKyEd1-SKqG37s6OTxucZAw", false, true);
 
+{
+    "name": ,
+    "email": ,
+    "password": "4444",
+    "location": ,
+    "interests": ,
+    "about": ,
+    "activities": ,
+    "profilePhoto": ,
+    "coverPhoto": ,
+    "username": 
+}
+-- 
+INSERT INTO register (name, email, password, handle, location, interests, activities, aboutYou, profilePhoto, coverPhoto, host, hostee) VALUES ("Donald D.", "donald_d@gmail.com", "?", "@donald", "Cape Town, South Africa", "dining and boats", "swimming, snorkeling, fishing, boating", "Hi I am Donald. I am traveling to Cape Town and want to explore, dine and play in the water", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxTv_ycswr8-VuWAKlrBSKCxgnc-npc5SJ7qH3tUZsSBmiIbIJIw", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4SVsIwVY-91nTU15aVxlJWZ_XrgsJzFtRknZKS8WXjb-hrA5tOw", false, true);
 
-
-//https://images.pexels.com/photos/1081685/pexels-photo-1081685.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940
-//3Motional
-
-//
-//
-
-//
-//
-
-//https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940
-//Shelly Struthers
-
-//https://images.pexels.com/photos/157661/young-woman-shooting-model-157661.jpeg?auto=compress&cs=tinysrgb&h=650&w=940
-//Sam Bee
-
-//https://images.pexels.com/photos/756453/pexels-photo-756453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940
-//Brucie Mars
-
-//https://images.pexels.com/photos/936229/pexels-photo-936229.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940
-//Matt Tdtgt
+{
+    "name": ,
+    "email": ,
+    "password": "4444",
+    "location": ,
+    "interests": ,
+    "about": ,
+    "activities": ,
+    "profilePhoto": ,
+    "coverPhoto": ,
+    "username": 
+}
