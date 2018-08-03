4e90ceacb829e15253025e1225118d733c7093a8
Elizaveta Onatsko <elizavetaonatsko@Elizavetas-MacBook-Pro.local>
2018-08-02 18:50:50 -0700

    inbox/outbox linked, everything else dummy data except for calendar; booking is empty:

--- public/assets/js/settings.js
+++ public/assets/js/settings.js
@@ -1,7 +1,6 @@
 $("#mail").hide();
 $("#calendar").hide();
 $("#booking").hide();
-$("#delete").hide();
 var postAgain = false;
 $("#inboxC").on("click", function () {
 
@@ -29,7 +28,6 @@ $("#profB").on("click", function () {
     $('#mailB').removeClass('selected');
     $('#calendarB').removeClass('selected');
     $('#bookingB').removeClass('selected');
-    $('#deleteB').removeClass('selected');
     $(this).addClass('selected');
     $("#profile").show();
     $("#mail").hide();
@@ -41,7 +39,6 @@ $("#mailB").on("click", function () {
     $('#profB').removeClass('selected');
     $('#calendarB').removeClass('selected');
     $('#bookingB').removeClass('selected');
-    $('#deleteB').removeClass('selected');
     $(this).addClass('selected');
     $("#mail").show();
     $("#profile").hide();
@@ -53,7 +50,6 @@ $("#calendarB").on("click", function () {
     $('#mailB').removeClass('selected');
     $('#profB').removeClass('selected');
     $('#bookingB').removeClass('selected');
-    $('#deleteB').removeClass('selected');
     $(this).addClass('selected');
     $("#calendar").show();
     $("#profile").hide();
@@ -65,7 +61,6 @@ $("#bookingB").on("click", function () {
     $('#mailB').removeClass('selected');
     $('#profB').removeClass('selected');
     $('#calendarB').removeClass('selected');
-    $('#deleteB').removeClass('selected');
     $(this).addClass('selected');
     $("#booking").show();
     $("#profile").hide();
@@ -74,19 +69,7 @@ $("#bookingB").on("click", function () {
     $("#delete").hide();
 
 });
-$("#deleteB").on("click", function () {
-    $('#mailB').removeClass('selected');
-    $('#profB').removeClass('selected');
-    $('#bookingB').removeClass('selected');
-    $('#calendarB').removeClass('selected');
-    $(this).addClass('selected');
-    $("#delete").show();
-    $("#profile").hide();
-    $("#mail").hide();
-    $("#calendar").hide();
-    $("#booking").hide();
 
-});
 
 
 //get all the messages 
